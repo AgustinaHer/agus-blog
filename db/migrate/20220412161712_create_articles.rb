0a21c3742c8c31b7d735362a2d45d40d525090fb
agustina.hernandez <agustina.hernandez@eva.edu.uy>
2022-04-12 15:12:03 -0300

    validation

--- db/migrate/20220412161712_create_articles.rb
+++ db/migrate/20220412161712_create_articles.rb
@@ -2,7 +2,6 @@ class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
     t.string :title
-    t.text :decription
+    t.text :description
     end
   end
-end
